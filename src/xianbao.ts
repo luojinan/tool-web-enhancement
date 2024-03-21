995904b2e77933d3fef2c05972bee549a1db058b
luojinan <luojinan@zhuanzhuan.com>
2024-03-21 21:01:56 +0800

    feat: 列表过滤不感兴趣

--- src/xianbao.ts
+++ src/xianbao.ts
@@ -1,4 +1,4 @@
-import { filterCommentText } from "./const"
+import { NOT_NEED_LIST, filterCommentText } from "./const"
 import { removeDomByList } from "./utils"
 
 const removeComment = () => {
@@ -19,9 +19,23 @@ const removeComment = () => {
 }
 
 export const xianbaoRun = (): number => {
-  const strList = ['.nav2-ul', '.article-list.top', '.pop-hongbao-on', '.tishi', '.xiangguan', 'aside', '#commentbox', 'footer']
+  const strList = ['.nav2-ul', '.article-list.top', '.pop-hongbao-on', '.tishi', '.xiangguan', 'aside', '#commentbox', '.footer']
   removeDomByList(strList)
   document.querySelector('.copyright')?.parentElement?.remove()
-  const count = removeComment()
+
+  let count = 0
+  const zoyeList = document.querySelectorAll('.article-list .title a')
+  if (zoyeList.length) {
+    zoyeList.forEach((item) => {
+      const dom = item as HTMLElement
+      const isNoNeed = NOT_NEED_LIST.some((noNeed) => dom.innerText.includes(noNeed))
+      if (isNoNeed) {
+        count += 1
+        dom.closest('.article-list')?.remove()
+      }
+    })
+  } else {
+    count = removeComment()
+  }
   return count
 }
